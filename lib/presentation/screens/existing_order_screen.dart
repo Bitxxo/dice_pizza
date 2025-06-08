2ce556e3958672c35e803a11596ed7057cddbb22
Leo-Carlos3 <2258466@alu.murciaeduca.es>
2025-06-08 10:23:05 +0200

    ya paro

--- lib/presentation/screens/existing_order_screen.dart
+++ lib/presentation/screens/existing_order_screen.dart
@@ -1,22 +1,17 @@
 import 'package:dice_pizza/presentation/widgets/order_bottom_navigation.dart';
 import 'package:flutter/material.dart';
 
-class ExistingOrderScreen extends StatefulWidget {
+class ExistingOrderScreen extends StatelessWidget {
   static const name = "EditOrder";
   const ExistingOrderScreen(this.child, {super.key});
   final Widget? child;
 
   @override
-  State<ExistingOrderScreen> createState() => _ExistingOrderScreenState();
-}
-
-class _ExistingOrderScreenState extends State<ExistingOrderScreen> {
-  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         bottomNavigationBar: OrderBottomNavigation(),
-        body: Center(child: widget.child ?? Placeholder()),
+        body: Center(child: child ?? Placeholder()),
       ),
     );
   }
